6602e7ce638f11c2c0bb11f23adc422a14f63c56
Kraken <106093343+Kraken-99@users.noreply.github.com>
2023-02-14 14:23:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// page to page
+
+function show(shown, hidden) {
+    document.getElementById='block';
+    document.getElementById='none';
+    return false;
+}
+
+// Alert
+
+function myAlert() {
+    alert("Cette page n'est pas disponible pour le moment.")
+}
